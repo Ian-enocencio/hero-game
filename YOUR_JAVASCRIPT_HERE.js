5ea45aebd638a527159861d97a83135520ee6b9d
Ian-enocencio <i.e.vandervlist@gmail.com>
2019-07-06 21:38:51 +0200

    things that doesnt work

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -28,14 +28,38 @@ function rest (hero) {
    return hero
 }
 
+const MockAlert = {
+    install: function() {
+        MockAlert.message = undefined;
+        MockAlert.originalAlert = window.alert;
+
+        window.alert = function(message) {
+            MockAlert.message = message;
+        }
+    },
+
+    unistall: function() {
+        window.alert = MockAlertoriginAlert;
+    }
+}
+
+MockAlert.install()
+rest(hero)
+MockAlert.unistall()
 // ---------- Picking up items 
 
 function pickUpItem  (hero) {
+
 
 }
 
 //---------- equip weapon 
 function equipWeapon (hero) {
+    
+    const testHero = { weapon: {type: 'sword', damage: 5}, inventory: [{ type: 'dagger', damage: 2}]}
+    const testWeapon = tesHero.inventory[0]
+    equipWeapon(testHero)
+    console.log(testHero);
 
 }
 
